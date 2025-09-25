51065a29b6bf84b74b16157bc93bebf0573cc7ea
molindu <molinduyasodha@gmail.com>
2025-09-25 22:30:08 +0530

    icon components

---
+++ src/components/icons/Minus.tsx
@@ -0,0 +1,16 @@
+
+const Plus = () => {
+    return (
+        <svg
+            width="11"
+            height="11"
+            xmlns="http://www.w3.org/2000/svg"
+            className="text-Purple-200 hover:text-Purple-600 cursor-pointer transition-colors duration-300 ease-in-out pt-1"
+        >
+            <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+                  fill="currentColor"/>
+        </svg>
+    );
+};
+
+export default Plus;
